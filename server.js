d5dc9d2acac2c2f5dac69151000b9f612cc0fd24
Joseph Onyach <josephokothonyach@gmail.com>
2024-01-15 14:03:34 +0300

    first commit

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-const connectDb = require("./connection/dbConnection");
-const Payment = require("./models/paymentmodel");
+// const connectDb = require("./connection/dbConnection");
+// const Payment = require("./models/paymentmodel");
 const app = express();
 
 app.use(express.json());
@@ -11,33 +11,59 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 
 //Mongodb connection
-connectDb()
+// connectDb()
 
 
 //routes
 //test endpoint to see if the generate token function works
 app.get('/tokencheck', (req, res) => {
-    generateToken();
+    // generateToken();
 })
 //create a generate token middleware
-const generateToken = async (req,res, next)=>{ 
-    const consumerKey =process.env.MPESA_CONSUMER_KEY
-    const consumerSecret = process.env.MPESA_CONSUMER_SECRET
-    const auth=new Buffer.from(`${consumerKey}:${consumerSecret}`).toString("base64");
-    try {
-        const response = await axios.get("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials", {
-            headers: {
-                authorization: `Basic ${auth}`,
-            }
-        });
-        console.log(response.data.access_token);
-         token = response.data.access_token;
-        next();
-    } catch (err) {
-        console.log(err.message);
-        // res.status(400).json(err.message);
-    }
-}
+// const generateToken = async (req,res, next)=>{ 
+//     const consumerKey =process.env.MPESA_CONSUMER_KEY
+//     const consumerSecret = process.env.MPESA_CONSUMER_SECRET
+//     const auth=new Buffer.from(`${consumerKey}:${consumerSecret}`).toString("base64");
+//     try {
+//         const response = await axios.get("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials", {
+//             headers: {
+//                 authorization: `Basic ${auth}`,
+//             }
+//         });
+//         console.log(response.data.access_token);
+//         token = response.data.access_token;
+//         next();
+//     } catch (err) {
+//         console.log(err.message);
+//         // res.status(400).json(err.message);
+//     }
+// }
+
+const generateToken = () => {
+    return new Promise((resolve, reject) => {
+      let endpoint_auth_credentials = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+      const auth=new Buffer.from(`${consumerKey}:${consumerSecret}`).toString("base64");
+      request(
+        {
+          uri: endpoint_auth_credentials,
+  
+          headers: {
+            Authorization: `Basic ${auth}`,
+          },
+        },
+        (err, response, body) => {
+          if (err) {
+            console.log(err);
+            reject(err);
+          } else {
+            const access_token = JSON.parse(body).access_token;
+            resolve(access_token);
+          }
+        }
+      );
+    });
+};
+  
 
 app.post('/mpesa',generateToken, async (req, res) => {
     const phone = req.body.phone.substring(1);
@@ -56,6 +82,7 @@ app.post('/mpesa',generateToken, async (req, res) => {
         const passkey = process.env.MPESA_PASS_KEY;
 
         const password = new Buffer.from(shortCode + passkey +timeStamp).toString("base64")
+
 
     // res.json({ phone: phone, amount: amount })Avoid sending multipe responses to the client
     await axios.post("https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
@@ -78,42 +105,15 @@ app.post('/mpesa',generateToken, async (req, res) => {
             },
         }
         ).then((response)=>{
-            console.log(response.data);
+            console.log("data",response.data);
             res.status(200).json(response.data);
         }).catch((err)=>{
-            console.log(err.message)
+            console.log(err)
             res.status(400).json(err.message)
         })
 
+
 });
-
-app.post("/callback",(req,res)=>{
-    const callbackData= req.body;
-    console.log(callbackData);
-    if(!callbackData.Body.stkCallback.CallbackMetadata){
-        console.log(callbackData.Body);
-        res.sendStatus(200)// Respond with a status of 200 to acknowledge receipt of the callback  
-    }
-
-    console.log(callbackData.Body.stkCallback.CallbackMetadata);
-    
-    const phoneNumber=callbackData.Body.stkCallback.CallbackMetadata.Item[4].Value;
-    const transaction_id=callbackData.Body.stkCallback.CallbackMetadata.Item[1].Value;
-    const amount=callbackData.Body.stkCallback.CallbackMetadata.Item[0].Value;
-
-    // console.log({phoneNumber,amount,transaction_id});
-    //initiate saving transaction information into the mongoose database
-    const payment = new Payment();
-    payment.phoneNumber = phoneNumber;
-    payment.amount = amount;
-    payment.transaction_id= transaction_id;
-    //save the info in the payment model
-    payment.save().then((data)=> {
-        console.log({message:"Saved successfully",data});
-    }).catch((err)=>{
-        console.log(err.message)
-    })
-})
 
 
 port = 8080 || process.env.PORT
